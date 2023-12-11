97d4efc357340372fd7010cee270574112a11be1
Cedric Daunke <daunke40@gmail.com>
2023-12-11 18:55:43 +0100

    added loading by ID

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let currentPokemon;
+let pokemonId = 1;
 
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log('Informations:', currentPokemon);
