1b40379590ab56c8683c3fc7b2c0353d3aa8cd88
Cedric Daunke <daunke40@gmail.com>
2023-12-18 18:25:28 +0100

    added new design and new logic

--- script.js
+++ script.js
@@ -6,5 +6,32 @@ async function loadPokemon() {
   let url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   let response = await fetch(url);
   currentPokemon = await response.json();
-  console.log("Informations:", currentPokemon);
+  console.log("Informations:", currentPokemon['types']['0']['type']['name']);
+}
+
+
+async function loadPokeCards() {
+await loadPokemon();
+let renderContainer = document.getElementById('render_container');
+renderContainer.innerHTML += renderPokeInfoCard(currentPokemon);
+}
+
+
+
+// renderfunctions
+
+function renderPokeInfoCard(currentPokemon) {
+return `
+<div class="pokeInfoCard">
+        <h1 id="PokeName">${currentPokemon['name']}</h1>
+        <div class="containerForPokeImg">
+          <div class="pokeCardShortInfo">
+            <div class="infoCardGrey"><p id="PokeType">${currentPokemon['types']['0']['type']['name']}</p></div>
+            <div class="infoCardGrey"><p id="PokeWeight">${currentPokemon['weight']} kg</p></div>
+          </div>
+          <img src="${currentPokemon['sprites']['front_shiny']}" alt="pokemonIMG" id="pokemonImg">
+        </div>
+      </div>
+    </div>
+`;
 }
