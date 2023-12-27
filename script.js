402f875ff9b8ce8a2e3624bb31ef7082178cc7c1
Cedric Daunke <daunke40@gmail.com>
2023-12-27 18:03:07 +0100

    Logic for open big PokeCard

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const MAX_POKEMON = 151;
+const MAX_POKEMON = 5;
 let currentPokemon;
 let pokemonId = 1;
+let allPokemon = [];
+var regExp = /[a-zA-Z]/g;
 
 let colors = {
   grass: '#46D1B1',
@@ -9,7 +11,7 @@ let colors = {
   electric: '#FFD970',
   poison:'#ae63b2',
   ground:'#baa76d',
-  rock:'545454',
+  rock:'#545454',
   ghost:'5a0096',
   ice:'#38eeff',
   normal: '#929292',
@@ -19,40 +21,74 @@ let colors = {
 async function loadPokemon() {
   let url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   let response = await fetch(url);
-  currentPokemon = await response.json();
-  console.log("Informations:", currentPokemon["types"]["0"]["type"]["name"]);
+  return await response.json();
 }
 
 async function loadPokeCards() {
   for (let i = 1; i <= MAX_POKEMON; i++) {
-    pokemonId = i;
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-    await loadPokemon(url);
+    let pkm = await loadPokemon(i);
+    pokemonId++;
+    allPokemon[i] = pkm; 
     let renderContainer = document.getElementById("render_container");
-    renderContainer.innerHTML += renderPokeInfoCard(currentPokemon);
-    colorOfInfoCard(i); // Übergabe des Index für die Klasse
+    renderContainer.innerHTML += renderPokeInfoCard(i);
+    colorOfInfoCard(i);
   }
 }
 
 function colorOfInfoCard(index) {
   let smallPokeCard = document.getElementsByClassName('smallPokeCard')[index - 1];
-  let pokemonType = currentPokemon["types"]["0"]["type"]["name"];
+  let pokemonType = allPokemon[index].types[0].type.name; 
   let backgroundColor = colors[pokemonType] || colors['default'];
   smallPokeCard.style.backgroundColor = backgroundColor;
 }
 
+function searchPokemon(currentPokemon) {
+  let searchingPokemon = currentPokemon['name'];
+  console.log(searchingPokemon);
+  let search = searchingPokemon.find()
+}
+
+async function openBigInfoCard(i) {
+  let pokemon = allPokemon[i];
+  let renderContainer = document.getElementById("render_container");
+  renderContainer.innerHTML = renderbigInfoCard(pokemon);
+}
+
+
+
 // renderfunctions
 
-function renderPokeInfoCard(currentPokemon) {
+function renderPokeInfoCard(i) {
+  let pokemon = allPokemon[i];
   return `
-<div class="pokeInfoCard smallPokeCard">
-  <h1 id="PokeName">${currentPokemon["name"]}</h1>
+    <div class="pokeInfoCard smallPokeCard" onclick="openBigInfoCard(${i})">
+      <h1 id="PokeName">${pokemon.name}</h1>
   <div class="containerForPokeImg">
     <div class="pokeCardShortInfo">
-      <div class="infoCardGrey"><p id="PokeType">${currentPokemon["types"]["0"]["type"]["name"]}</p></div>
-      <div class="infoCardGrey"><p id="PokeWeight">${currentPokemon["weight"]} kg</p></div>
+      <div class="infoCardGrey"><p id="PokeType">${pokemon.types[0].type.name}</p></div>
+      <div class="infoCardGrey"><p id="PokeWeight">${pokemon.weight} kg</p></div>
     </div>
-    <img src="${currentPokemon["sprites"]["front_shiny"]}" alt="pokemonIMG" id="pokemonImg">
+    <img src="${pokemon.sprites.front_shiny}" alt="pokemonIMG" id="pokemonImg">
   </div>
 </div>`;
+}
+
+ function renderbigInfoCard(pokemon) {
+  return `
+  <div class="renderBigInfo_container">
+        <div class="bigInfo_Container">
+          <img src="${pokemon.sprites.front_shiny}" alt="pokemon" class="pokeImgBigInfo">
+        <div class="pokemonInfoContainer">
+          <div class="pokeStatsHeader">
+            <div id="about">
+              <h2>About</h2>
+            </div>
+            <div id="BaseStats">
+              <h2>BaseStats</h2>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+  `;
 }
