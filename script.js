13b219d4f565d27cd7418afe686e047dff423401
Cedric Daunke <daunke40@gmail.com>
2023-12-29 16:56:54 +0100

    added baseStats and responsive

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let allPokemon = [];
 var regExp = /[a-zA-Z]/g;
 
 let colors = {
-  grass: '#46D1B1',
-  fire: '#FA6555',
-  water: '#76BDFD',
-  electric: '#FFD970',
-  poison:'#ae63b2',
-  ground:'#baa76d',
-  rock:'#545454',
-  ghost:'5a0096',
-  ice:'#38eeff',
-  normal: '#929292',
-  default: '#3d3d3d'
+  grass: "#46D1B1",
+  fire: "#FA6555",
+  water: "#76BDFD",
+  electric: "#FFD970",
+  poison: "#ae63b2",
+  ground: "#baa76d",
+  rock: "#545454",
+  ghost: "5a0096",
+  ice: "#38eeff",
+  normal: "#929292",
+  default: "#3d3d3d",
 };
 
 async function loadPokemon() {
@@ -29,47 +29,53 @@ async function loadPokeCards() {
   for (let i = 1; i <= MAX_POKEMON; i++) {
     let pkm = await loadPokemon(i);
     pokemonId++;
-    allPokemon[i] = pkm; 
+    allPokemon[i] = pkm;
     let renderContainer = document.getElementById("render_container");
     renderContainer.innerHTML += renderPokeInfoCard(i);
     colorOfInfoCard(i);
   }
-  
 }
 
 function colorOfInfoCard(index) {
-  let smallPokeCard = document.getElementsByClassName('smallPokeCard')[index - 1];
-  let pokemonType = allPokemon[index].types[0].type.name; 
-  let backgroundColor = colors[pokemonType] || colors['default'];
+  let smallPokeCard =
+    document.getElementsByClassName("smallPokeCard")[index - 1];
+  let pokemonType = allPokemon[index].types[0].type.name;
+  let backgroundColor = colors[pokemonType] || colors["default"];
   smallPokeCard.style.backgroundColor = backgroundColor;
 }
 
 function searchPokemon(currentPokemon) {
-  let searchingPokemon = currentPokemon['name'];
+  let searchingPokemon = currentPokemon["name"];
   console.log(searchingPokemon);
-  let search = searchingPokemon.find()
+  let search = searchingPokemon.find();
 }
 
 async function openBigInfoCard(i) {
-  let pokemon = allPokemon[i];
   let renderContainer = document.getElementById("render_container");
-  renderContainer.innerHTML = renderbigInfoCard(pokemon);
-  aboutInformation(pokemon);
+  renderContainer.innerHTML = renderbigInfoCard(i);
+  aboutInformation(i);
 }
 
 function closeBigInfoCard(event) {
   if (event) event.stopPropagation();
 
-  let element = document.getElementById('closingContainer');
-  element.style.display = 'none';
+  let element = document.getElementById("closingContainer");
+  element.style.display = "none";
   loadPokeCards();
 }
 
 // pokemonInformation functions
 
-function aboutInformation(pokemon) {
-  let infoContainer = document.getElementById('renderInformations');
+function aboutInformation(i) {
+  let infoContainer = document.getElementById("renderInformations");
+  let pokemon = allPokemon[i];
   infoContainer.innerHTML = renderAboutInformation(pokemon);
+}
+
+function baseStatsInformation(i) {
+  let infoContainer = document.getElementById("renderInformations");
+  let pokemon = allPokemon[i];
+  infoContainer.innerHTML = renderBaseStats(pokemon);
 }
 
 // renderfunctions
@@ -89,17 +95,19 @@ function renderPokeInfoCard(i) {
 </div>`;
 }
 
-function renderbigInfoCard(pokemon) {
+function renderbigInfoCard(i) {
+  let pokemon = allPokemon[i];
+
   return `
   <div class="renderBigInfo_container" id="closingContainer" onclick="closeBigInfoCard(event)">
         <div class="bigInfo_Container" onclick="event.stopPropagation()">
           <img src="${pokemon.sprites.front_shiny}" alt="pokemon" class="pokeImgBigInfo">
         <div class="pokemonInfoContainer">
           <div class="pokeStatsHeader">
-            <div id="about" onclick="aboutInformation(${pokemon})">
+            <div id="about" onclick="aboutInformation(${i})"> 
               <h2>About</h2>
             </div>
-            <div id="BaseStats" onclick="BaseStatsInformation()">
+            <div id="BaseStats" onclick="baseStatsInformation(${i})"> 
               <h2>BaseStats</h2>
             </div>
           </div>
@@ -112,13 +120,56 @@ function renderbigInfoCard(pokemon) {
 }
 
 function renderAboutInformation(pokemon) {
-  
   console.log(pokemon);
   return `
   <div class="aboutInformation">
    <div class="infosContainer"><p class="infoHeadline">Name:</p> <p class="infos">${pokemon.name}</p></div>
    <div class="infosContainer"><p class="infoHeadline">Type:</p> <p class="infos">${pokemon.types[0].type.name}</p></div>
    <div class="infosContainer"><p class="infoHeadline">Weight:</p><p class="infos">${pokemon.weight} kg </p></div>
+  </div>
+  `;
+}
+
+function renderBaseStats(pokemon) {
+  console.log(pokemon);
+  return `
+  <div class="baseStatsInfo_Container">
+  <div class="infoBar">
+  <p class="infoText">HP</p>
+  <div id="baseStatsInfo">
+  <div id="percentBar" style="width: ${pokemon.stats[0].base_stat}%"></div>
+  </div>
+  </div>
+  <div class="infoBar">
+  <p class="infoText">attack</p>
+  <div id="baseStatsInfo">
+  <div id="percentBar" style="width: ${pokemon.stats[1].base_stat}%"></div>
+  </div>
+  </div>
+  <div class="infoBar">
+  <p class="infoText">defense</p>
+  <div id="baseStatsInfo">
+  <div id="percentBar" style="width: ${pokemon.stats[2].base_stat}%"></div>
+  </div>
+  </div>
+  <div class="infoBar">
+  <p class="infoText">special-attack</p>
+  <div id="baseStatsInfo">
+  <div id="percentBar" style="width: ${pokemon.stats[3].base_stat}%"></div>
+  </div>
+  </div>
+  <div class="infoBar">
+  <p class="infoText">special-defense</p>
+  <div id="baseStatsInfo">
+  <div id="percentBar" style="width: ${pokemon.stats[4].base_stat}%"></div>
+  </div>
+  </div>
+  <div class="infoBar">
+  <p class="infoText">speed</p>
+  <div id="baseStatsInfo">
+  <div id="percentBar" style="width: ${pokemon.stats[5].base_stat}%"></div>
+  </div>
+  </div>
   </div>
   `;
 }
